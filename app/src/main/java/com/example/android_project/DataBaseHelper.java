e196e42061ecc36f26b7f7dc8eb4500853c80be6
jiangkeer-art <130060060@qq.com>
2022-06-17 17:05:49 +0800

    数据库(加upgrade）

--- app/src/main/java/com/example/android_project/DataBaseHelper.java
+++ app/src/main/java/com/example/android_project/DataBaseHelper.java
@@ -7,63 +7,127 @@ import android.widget.Toast;
 
 public class DataBaseHelper extends SQLiteOpenHelper {
 
+    /*
+    用户表：
+    用户名（电话号码）（主键）
+    密码
+    是否为管理员
+    */
     public static final String CREATE_USER = "Create table User("
-            + "phone varchar(255) primary key,"
-            + "password varchar(255),"
-            + "is_administrators integer)";
+            + "phone varchar(12) primary key not null,"
+            + "password varchar(64),"
+            + "is_administrators BLOB)";
 
+    /*
+    普通用户表：
+    用户名（电话号码）（主键）
+    昵称
+    姓名
+    身份证号
+    外键用户名连接到用户表的用户名
+     */
     public static final String CREATE_NORMAL_USER = "Create table Normal_User("
-            + "phone varchar(255) primary key,"
-            + "id varchar(255),"
-            + "name varchar(255),"
-            + "id_number varchar(255),"
+            + "phone varchar(12) primary key not null,"
+            + "id varchar(16),"
+            + "name varchar(5),"
+            + "id_number varchar(24),"
             + "foreign key (phone) references User(phone))";
 
+    /*
+    管理员用户表
+    用户名（电话号码）（主键）
+    昵称
+    外键用户名连接到用户表的用户名
+     */
     public static final String CREATE_ADMINISTRATORS_USER = "Create table Administrators_User("
-            + "phone varchar(255) primary key,"
-            + "id varchar(255),"
+            + "phone varchar(12) primary key not null,"
+            + "id varchar(16),"
             + "foreign key (phone) references User(phone))";
 
+    /*
+    订单表：
+    订单编号（主键）
+    用户名（电话号码）
+    航班编号
+    起飞时间
+    状态（是否过期）
+    外键用户名连接到用户表的用户名
+    外键航班编号连接到航班表的航班编号
+    外键起飞时间连接到航班表的起飞时间
+     */
     public static final String CREATE_MY_ORDER = "Create table My_Order("
             + "order_number integer primary key autoincrement,"
-            + "phone varchar(255),"
-            + "flight_number varchar(255),"
-            + "takeoff_time varchar(255),"
-            + "expired integer,"
+            + "phone varchar(12),"
+            + "flight_number varchar(10),"
+            + "takeoff_time datetime,"
+            + "expired BLOB,"
             + "foreign key (phone) references User(phone),"
-            + "foreign key (flight_number) references Flight(flight_number),"
-            + "foreign key (takeoff_time) references Flight(takeoff_time))";
+            + "foreign key (flight_number,takeoff_time) references Flight(flight_number,takeoff_time))";
 
+    /*
+    航班表：
+    航班编号
+    国内/国际
+    起飞城市
+    降落城市
+    中专城市
+    价格
+    起飞时间
+    舱位
+    准点率
+    是否直飞
+    是否共享
+    时间段
+    航空公司
+    餐食
+    起飞航站楼
+    降落航站楼
+    主键：航班编号，起飞时间
+    外键起飞城市连接到城市表的城市名
+    外键降落城市连接到城市表的城市名
+    外键中转城市连接到城市表的城市名
+    外键航空公司连接到航空公司表的公司名
+     */
     public static final String CREATE_FLIGHT = "Create table Flight("
-            + "flight_number varchar(255),"
-            + "is_domestic integer,"
-            + "takeoff_city varchar(255),"
-            + "landing_city varchar(255),"
-            + "transit_city varchar(255),"
+            + "flight_number varchar(10) not null,"
+            + "is_domestic BLOB,"
+            + "takeoff_city varchar(15),"
+            + "landing_city varchar(15),"
+            + "transit_city varchar(15),"
             + "price integer,"
-            + "takeoff_time varchar(255),"
+            + "takeoff_time datetime,"
             + "shipping_space varchar(255),"
             + "punctuality_rate real,"
-            + "is_direct_flight integer,"
-            + "is_share integer,"
-            + "time_period varchar(255),"
-            + "airline_company varchar(255),"
-            + "food varchar(255),"
-            + "departure_terminal varchar(255),"
-            + "landing_terminal varchar(255),"
+            + "is_direct_flight BLOB,"
+            + "is_share BLOB,"
+            + "time_period varchar(8),"
+            + "airline_company varchar(15),"
+            + "food BLOB,"
+            + "departure_terminal varchar(15),"
+            + "landing_terminal varchar(15),"
             + "primary key (flight_number,takeoff_time),"
             + "foreign key (takeoff_city) references City(city_name),"
             + "foreign key (landing_city) references City(city_name),"
             + "foreign key (transit_city) references City(city_name),"
             + "foreign key (airline_company) references Airline_Company(company_name))";
 
+    /*
+    城市表：
+    城市编号（主键）
+    城市名
+     */
     public static final String CREATE_CITY = "Create table City("
             + "city_number integer primary key autoincrement,"
-            + "city_name varchar(255))";
+            + "city_name varchar(15))";
 
+    /*
+    航空公司表：
+    公司编号（主键）
+    公司名
+     */
     public static final String CREATE_AIRLINE_COMPANY = "Create table Airline_Company("
             + "company_number integer primary key autoincrement,"
-            + "company_name varchar(255))";
+            + "company_name varchar(15))";
 
     private Context mContext;
     public DataBaseHelper(Context context,String name,SQLiteDatabase.CursorFactory factory,int version){
@@ -71,6 +135,7 @@ public class DataBaseHelper extends SQLiteOpenHelper {
         mContext = context;
     }
 
+    //打开SQLite的外键功能
     @Override
     public void onOpen(SQLiteDatabase db){
         super.onOpen(db);
@@ -81,6 +146,7 @@ public class DataBaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        //创建各个表
         sqLiteDatabase.execSQL(CREATE_USER);
         sqLiteDatabase.execSQL(CREATE_NORMAL_USER);
         sqLiteDatabase.execSQL(CREATE_ADMINISTRATORS_USER);
@@ -88,9 +154,11 @@ public class DataBaseHelper extends SQLiteOpenHelper {
         sqLiteDatabase.execSQL(CREATE_FLIGHT);
         sqLiteDatabase.execSQL(CREATE_CITY);
         sqLiteDatabase.execSQL(CREATE_AIRLINE_COMPANY);
+        //创建成功时发出成功消息
         Toast.makeText(mContext, "DataBase Create Succeeded", Toast.LENGTH_SHORT).show();
     }
 
+    //数据库更新时的代码
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         sqLiteDatabase.execSQL("drop table if exists User");
--- app/src/main/java/com/example/android_project/MainActivity.java
+++ app/src/main/java/com/example/android_project/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,7 +14,119 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        dbHelper = new DataBaseHelper(this,"FlightDataBase.dp",null,1);
-        dbHelper.getWritableDatabase();
+        dbHelper = new DataBaseHelper(this,"FlightDataBase.dp",null,6);
+
+        //创建数据库并向其中添加数据
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        addUserData(db);
+        addCityData(db);
+        addCompanyData(db);
+        addFlightData(db);
+        addAdministratorsUserData(db);
+        addNormalUserData(db);
+        addOrderData(db);
+    }
+
+    //添加管理员用户数据函数
+    public void addAdministratorsUserData(SQLiteDatabase db){
+        ContentValues valueAdministrators_User=new ContentValues();
+
+        //管理员表
+        valueAdministrators_User.put("phone","18222279903");
+        valueAdministrators_User.put("id","Lord");
+        db.insert("Administrators_User",null,valueAdministrators_User);
+    }
+
+    //添加普通用户数据函数
+    public void addNormalUserData(SQLiteDatabase db){
+        ContentValues valueNormal_User=new ContentValues();
+
+        valueNormal_User.put("phone","18333378803");
+        valueNormal_User.put("id","FirstTest");
+        valueNormal_User.put("name","二蛋");
+        valueNormal_User.put("id_number","145685400109835618");
+        db.insert("Normal_User",null,valueNormal_User);
+
+        valueNormal_User.put("phone","18777777703");
+        valueNormal_User.put("id","SecondTest");
+        valueNormal_User.put("name","三蛋");
+        valueNormal_User.put("id_number","175686400209635415");
+        db.insert("Normal_User",null,valueNormal_User);
+    }
+
+    //添加用户数据函数
+    public void addUserData(SQLiteDatabase db){
+        ContentValues valueUser=new ContentValues();
+
+        valueUser.put("phone","18222279903");
+        valueUser.put("password","1215225");
+        valueUser.put("is_administrators","1"); //1为管理员，0为普通用户
+        db.insert("User",null,valueUser);
+
+        valueUser.put("phone","18333378803");
+        valueUser.put("password","123369");
+        valueUser.put("is_administrators","0");
+        db.insert("User",null,valueUser);
+
+        valueUser.put("phone","18777777703");
+        valueUser.put("password","321147");
+        valueUser.put("is_administrators","0");
+        db.insert("User",null,valueUser);
+    }
+
+    //添加订单数据函数
+    public void addOrderData(SQLiteDatabase db){
+        ContentValues valueOrderData = new ContentValues();
+
+        valueOrderData.put("phone","18333378803");
+        valueOrderData.put("flight_number","CA88");
+        valueOrderData.put("takeoff_time","2022-09-17");
+        valueOrderData.put("expired","1");
+        db.insert("My_Order",null,valueOrderData);
+    }
+
+    //添加航班数据函数
+    public void addFlightData(SQLiteDatabase db){
+        ContentValues valueFlightData = new ContentValues();
+
+        valueFlightData.put("flight_number","CA88");
+        valueFlightData.put("is_domestic","1"); //1为国内，0为国外
+        valueFlightData.put("takeoff_city","chengdu");
+        valueFlightData.put("landing_city","shanghai");
+        valueFlightData.put("transit_city","null");
+        valueFlightData.put("price","500");
+        valueFlightData.put("takeoff_time","2022-09-17");
+        valueFlightData.put("shipping_space","009-A");
+        valueFlightData.put("punctuality_rate","0.99");
+        valueFlightData.put("is_direct_flight","1");    //1为直飞，0为中转
+        valueFlightData.put("is_share","1");    //1为共享，0为不共享
+        valueFlightData.put("time_period","3h");
+        valueFlightData.put("airline_company","hangkong");
+        valueFlightData.put("food","1");
+        valueFlightData.put("departure_terminal","test1");
+        valueFlightData.put("landing_terminal","test2");
+        db.insert("Flight",null,valueFlightData);
+    }
+
+    //添加城市数据
+    public void addCityData(SQLiteDatabase db){
+        ContentValues valueCityData = new ContentValues();
+
+        valueCityData.put("city_name","null");
+        db.insert("City",null,valueCityData);
+
+        valueCityData.put("city_name","chengdu");
+        db.insert("City",null,valueCityData);
+
+        valueCityData.put("city_name","shanghai");
+        db.insert("City",null,valueCityData);
+    }
+
+    //添加航空公司数据
+    public void addCompanyData(SQLiteDatabase db){
+        ContentValues valueCompanyData = new ContentValues();
+
+        valueCompanyData.put("company_name","hangkong");
+        db.insert("Airline_Company",null,valueCompanyData);
     }
 }
