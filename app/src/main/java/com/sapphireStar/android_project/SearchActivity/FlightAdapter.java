c927c76cea5ffa7bc59ebb5915578f9ee6160116
jiangkeer-art <130060060@qq.com>
2022-06-23 23:02:15 +0800

    增加了search——out

--- app/src/main/java/com/sapphireStar/android_project/SearchActivity/FlightAdapter.java
+++ app/src/main/java/com/sapphireStar/android_project/SearchActivity/FlightAdapter.java
@@ -1,5 +1,6 @@
 package com.sapphireStar.android_project.SearchActivity;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -37,9 +38,33 @@ public class FlightAdapter extends RecyclerView.Adapter<FlightAdapter.ViewHolder
         String takeoff_time_string = flight.getTakeoff_time().toString();
         char[] time = takeoff_time_string.toCharArray();
         char[] time2 = Arrays.copyOfRange(time,11,16);
-        holder.takeoff_time.setText(Arrays.toString(time2));
+        takeoff_time_string = new String(time2);
+        holder.takeoff_time.setText(takeoff_time_string);
         holder.departure_terminal.setText(flight.getDeparture_terminal());
         holder.landing_terminal.setText(flight.getLanding_terminal());
+
+        String ssm,all;
+        StringBuffer h =new StringBuffer();
+        StringBuffer m =new StringBuffer();
+        h.insert(0,time2[0]);
+        h.insert(1,time2[1]);
+        m.insert(0,time2[3]);
+        m.insert(1,time2[4]);
+        int sh = Integer.parseInt(h.toString());
+        int PT = Integer.valueOf(flight.getTime_period()).intValue();
+        sh += PT/60;
+        String ssh = String.valueOf(sh);
+        int sm = Integer.parseInt(m.toString());
+        sm += PT % 60;
+        if(sm<10){
+            ssm = String.valueOf(sm);
+            all = ssh +":"+ "0"+ssm ;
+        }else {
+            ssm = String.valueOf(sm);
+            all = ssh + ":" + ssm;
+        }
+        holder.landing_time.setText(all);
+
     }
 
     @Override
@@ -48,7 +73,7 @@ public class FlightAdapter extends RecyclerView.Adapter<FlightAdapter.ViewHolder
     }
 
     static class ViewHolder extends RecyclerView.ViewHolder{
-        public TextView flight_number,air_company,takeoff_time,departure_terminal,landing_terminal;
+        public TextView flight_number,air_company,takeoff_time,departure_terminal,landing_terminal,landing_time;
         public ViewHolder(View itemView) {
             super(itemView);
             flight_number = itemView.findViewById(R.id.flight_number);
@@ -56,6 +81,8 @@ public class FlightAdapter extends RecyclerView.Adapter<FlightAdapter.ViewHolder
             takeoff_time = itemView.findViewById(R.id.takeoff_time);
             departure_terminal = itemView.findViewById(R.id.departure_terminal);
             landing_terminal = itemView.findViewById(R.id.landing_terminal);
+            landing_time = itemView.findViewById(R.id.landing_time);
+
         }
     }
 }
