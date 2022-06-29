f21c17ed7a9faecb942dc0ae5eed72d28d25ab76
jiangkeer-art <130060060@qq.com>
2022-06-29 15:17:45 +0800

    添加了我的收藏功能

--- app/src/main/java/com/sapphireStar/android_project/SearchActivity/FlightAdapter.java
+++ app/src/main/java/com/sapphireStar/android_project/SearchActivity/FlightAdapter.java
@@ -118,7 +118,7 @@ public class FlightAdapter extends RecyclerView.Adapter<FlightAdapter.ViewHolder
         holder.price.setText("¥:"+planeTicket.getPrice());
 
         if(mmyAttentions!=null) {
-            Toast.makeText(mContext, "addssdasdasdasda succession", Toast.LENGTH_SHORT).show();
+            //Toast.makeText(mContext, "addssdasdasdasda succession", Toast.LENGTH_SHORT).show();
             PlaneTicket planeTicket1;
             for (int i = 0; i < mmyAttentions.size(); i++) {
                 planeTicket1=mmyAttentions.get(i);
